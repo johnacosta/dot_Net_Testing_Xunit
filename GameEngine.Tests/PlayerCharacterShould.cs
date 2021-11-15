0db520f66c896a5bfa9541ca95c58aa02b6e4acd
johnacosta <johnmd89@gmail.com>
2021-11-14 19:14:28 -0500

    Add project files.

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace GameEngine.Tests;
+
+public class PlayerCharacterShould
+{
+    [Fact]
+    public void BeInexperiencedWhenNew()
+    {   //ARRANGE
+        PlayerCharacter sut = new PlayerCharacter();
+
+        //ACT
+
+        //ASSERT 
+    }
+}
