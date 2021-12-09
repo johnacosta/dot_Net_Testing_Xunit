72f32b675eddfe2178a32b6e0662fb78d446cad7
John Acosta <john.acosta@citrix.com>
2021-12-08 22:10:50 -0500

    Add external test data

---
+++ GameEngine.Tests/ExternalHealthDamageTestData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Tests
+{
+    public class ExternalHealthDamageTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                string[] csvLines = File.ReadAllLines("TestData.csv");
+
+                var testCases = new List<object[]>();
+
+                foreach (var line in csvLines)
+                {
+                    IEnumerable<int> values = line.Split(',').Select(int.Parse);
+
+                    object[] testCase = values.Cast<object>().ToArray();
+
+                    testCases.Add(testCase);
+                }
+
+                return testCases;
+            }
+        }
+      
+    }
+}
--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -5,7 +5,8 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {
         [Theory]
-        [MemberData(nameof(InternalHealthDamageTestData.TestData), MemberType = typeof(InternalHealthDamageTestData))]
+        [MemberData(nameof(ExternalHealthDamageTestData.TestData), MemberType = typeof(ExternalHealthDamageTestData))]
+        //[MemberData(nameof(InternalHealthDamageTestData.TestData), MemberType = typeof(InternalHealthDamageTestData))]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
